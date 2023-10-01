fa1cc781a265e3800844f4690abd79a6bcfed22f
Murad Aliyev <98873214+murad-aliyev@users.noreply.github.com>
2023-10-01 21:15:04 +0400

    last commit

--- main.js
+++ main.js
@@ -1,30 +1,46 @@
 let searchInput = document.querySelector('.searchInput');
 
+let cityName = document.querySelector('h1');
 let temp = document.querySelector('.temp');
 let wind = document.querySelector('.wind');
 let humid = document.querySelector('.humid');
+let button = document.querySelector('.searchButton');
 
 
-let city = "baku";
-searchInput.addEventListener( "keyup", function (e) {
-    city= e.target.value;
-})
-
+let city='baku';
 let apiKey = "55589aad896342d3bad0dc0c64f9c3a0";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-fetch(url)
-  .then((response) => {
-    return response.json();
+
+function takeData() {
+
+  if (searchInput.value) {
+    city = searchInput.value;
+  }
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+
+  fetch(url)
+  .then((res) => {
+    return res.json();
   })
   .then((data) => {
     let info = data;
 
-    console.log(info.main.temp);
+    console.log(info);
 
-    temp.innerHTML= "Temperatur: " + Math.round(info.main.temp) + "°C"
+    cityName.innerHTML= info.name;
+    temp.innerHTML= "Temperatur: " + Math.round(info.main.temp) + "°C";
+    wind.innerHTML= "Külək: " + Math.round(info.wind.speed) + " km/saat";
+    humid.innerHTML= "Rütubət: " + Math.round(info.main.humidity) + "%";
 
   })
   .catch(function(error) {
-    console.log(error);
+    cityName.innerHTML= "Şəhər tapılmadı";
+    temp.innerHTML=  "Şəhər tapılmadı";
+    wind.innerHTML= "Şəhər tapılmadı";
+    humid.innerHTML=  "Şəhər tapılmadı";
   });
+}
+
+document.addEventListener('DOMContentLoaded', () => { takeData() })
+button.addEventListener('click', () => { takeData() })
